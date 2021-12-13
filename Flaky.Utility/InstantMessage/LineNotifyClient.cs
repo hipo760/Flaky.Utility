93615a8f2b0173a9ea551086bee501ecec9ddb83
hipo760 <hipo760@outlook.com>
2021-12-13 22:55:26 +0800

    fix: make line notify client public

--- Flaky.Utility/InstantMessage/LineNotifyClient.cs
+++ Flaky.Utility/InstantMessage/LineNotifyClient.cs
@@ -7,51 +7,48 @@ using System.Threading.Tasks;
 
 namespace Flaky.Utility.InstantMessage
 {
-    internal class LineNotifyClient
+    public class LineNotifyClient
     {
-        public class LineNotify
-        {
-            private static HttpClient _client;
-            public string Token { get; set; }
-            public string Url { get; set; }
-            public string PrefixMsg { get; set; }
+        private static HttpClient _client;
+        public string Token { get; set; }
+        public string Url { get; set; }
+        public string PrefixMsg { get; set; }
 
-            public LineNotify(
-                string token, 
-                string url = "https://notify-api.line.me/api/notify", 
-                string prefixMsg = "",
-                int httpClientTimeoutSec = 30)
+        public LineNotifyClient(
+            string token,
+            string url = "https://notify-api.line.me/api/notify",
+            string prefixMsg = "",
+            int httpClientTimeoutSec = 30)
+        {
+            Url = url;
+            PrefixMsg = prefixMsg;
+            Token = token;
+            _client = new HttpClient() { Timeout = TimeSpan.FromSeconds(httpClientTimeoutSec) };
+        }
+        public async Task SendMessageAsync(string msg)
+        {
+            var cts = new CancellationTokenSource();
+            try
             {
-                Url = url;
-                PrefixMsg = prefixMsg;
-                Token = token;
-                _client = new HttpClient() { Timeout = TimeSpan.FromSeconds(httpClientTimeoutSec) };
-            }
-            public async Task SendMessageAsync(string msg)
-            {
-                var cts = new CancellationTokenSource();
-                try
+                var values = new Dictionary<string, string>() { { "message", PrefixMsg + msg } };
+                var content = new FormUrlEncodedContent(values);
+                var httpRequestMessage = new HttpRequestMessage
                 {
-                    var values = new Dictionary<string, string>() { { "message", PrefixMsg + msg } };
-                    var content = new FormUrlEncodedContent(values);
-                    var httpRequestMessage = new HttpRequestMessage
-                    {
-                        Method = HttpMethod.Post,
-                        RequestUri = new Uri(Url),
-                        Headers = {
+                    Method = HttpMethod.Post,
+                    RequestUri = new Uri(Url),
+                    Headers = {
                         { HttpRequestHeader.Authorization.ToString(), $"Bearer {Token}" },
                     },
-                        Content = content
-                    };
-                    var response = await _client.SendAsync(httpRequestMessage, cts.Token);
+                    Content = content
+                };
+                var response = await _client.SendAsync(httpRequestMessage, cts.Token);
 
-                }
-                catch (Exception e)
-                {
-                    //Log.Logger.Error("[LineNotify.SendMessageAsync] Exception: {e}", e);
-                    cts?.Cancel();
-                    cts?.Dispose();
-                }
+            }
+            catch (Exception e)
+            {
+                //Log.Logger.Error("[LineNotify.SendMessageAsync] Exception: {e}", e);
+                cts?.Cancel();
+                cts?.Dispose();
             }
         }
     }
